a0ca539ebd4814e88fa46a7371fccdce36340ab6
Temenuzhka Gadzheva <temenujka_211@abv.bg>
2021-06-07 18:56:04 +0300

    Employees

---
+++ Employees/Employees/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+   public class Employee
+    {
+        private readonly List<Employee> employees;
+        private string id;
+        private string name;
+        private decimal salary;
+        public Employee()
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Salary = salary;
+            this.employees = new List<Employee>();
+        }
+        public string ID { get => this.id; set { this.id = value; } }
+        public string Name { get => this.name; set { this.name = value; } }
+        public decimal Salary { get => this.salary; set { this.salary = value; } }
+
+
+        public void AddEmployee(Employee employee)
+        {
+            if (this.employees.Contains(employee))
+            {
+                throw new InvalidOperationException($"Employee with id:{employee.ID}already exist!");
+            }
+            this.employees.Add(employee);
+        }
+        public List<Employee> ShowEmployees => this.employees;
+        public Employee ShowById(string id)
+        {
+            return this.employees.FirstOrDefault(e => e.ID == id);
+        }
+        public List<Employee> ShowBySalary(decimal salary)
+        {
+            return this.employees.Where(e => e.Salary > salary).ToList();
+        }
+        public void Save(BinaryWriter writer)
+        {
+            writer.Write(ID);
+            writer.Write(Name);
+            writer.Write(Salary);
+
+        }
+
+        public static Employee Read(BinaryReader reader)
+        {
+            Employee employee = new Employee();
+
+            employee.ID = reader.ToString();
+            employee.Name = reader.ToString();
+            employee.Salary = reader.ReadDecimal();
+
+            return employee;
+        }
+    }
+}
+
---
+++ Employees/Employees/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+             {
+                 new Employee() { ID = "123",Name = "Alex", Salary = 2500 },
+                 new Employee() {  ID = "345",Name = "Stamat", Salary = 3800},
+                 new Employee() {  ID = "567",Name = "Marty", Salary = 1900 },
+                 new Employee() {  ID = "789",Name = "Pepy", Salary = 7000 },
+             };
+            // SaveToFile(employees, @"D:\OOP2\Demo3105");
+            // Print(employees);
+        }
+        static void Print(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"Id: {employee.ID}, Name:{employee.Name}, salary:{employee.Salary}");
+            }
+            Console.WriteLine("---------------------------");
+        }
+        static void SaveToFile(List<Employee> employees, string fileName)
+        {
+            try
+            {
+                using (Stream fs = File.Create(fileName))
+                {
+                    using (var writer = new BinaryWriter(fs))
+                    {
+                        writer.Write(employees.Count);
+                        foreach (var employee in employees)
+                        {
+                            employee.Save(writer);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error writing file: {ex.Message}");
+            }
+        }
+
+        static List<Employee> readLineFromFile(string fileNmae)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                using (Stream fs = File.OpenRead(fileNmae))
+                {
+                    using (BinaryReader reader = new BinaryReader(fs))
+                    {
+                        int count = reader.ReadInt32();
+                        for (int i = 0; i < count; i++)
+                        {
+                            employees.Add(Employee.Read(reader));
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file:{ex.Message}");
+            }
+            return employees;
+        }
+    }
+}
